d36d1970482a171e4f8d405d447f14293451fb44
Alexis Josephides <ajosephides@gmail.com>
2019-07-16 17:34:25 +0100

    db: migration created to add column to posts of foreign key of user. cannot add posts directly now so tests breaking. need to rewrite create post tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190716114047) do
+ActiveRecord::Schema.define(version: 20190716151839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190716114047) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +32,5 @@ ActiveRecord::Schema.define(version: 20190716114047) do
     t.index ["encrypted_email"], name: "index_users_on_encrypted_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
