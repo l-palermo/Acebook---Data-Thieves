071b36626ed76fe00227a4b494fb17d2b0b563d3
Luigi <l-palermo@hotmail.it>
2019-07-19 17:05:40 +0100

    Checkout to devise branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name,        presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,        presence: true
   validates :email,       presence: true, format: { with:VALID_EMAIL_REGEX }
   validates :password,    presence: true, length: { maximum: 10, minimum: 6 }
 
