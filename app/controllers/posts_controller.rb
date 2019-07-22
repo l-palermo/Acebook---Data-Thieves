c396bac3f234647c9a4b0557f8b413ff5c31468a
Luigi <l-palermo@hotmail.it>
2019-07-22 13:02:47 +0100

    development: Commented out tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,18 @@
 class PostsController < ApplicationController
   def new
+    redirect_to '/' if session[:user_id] == nil
     @post = Post.new
   end
 
   def create
+    redirect_to '/' if session[:user_id] == nil
     @post = Post.create(post_params)
     redirect_to posts_url
   end
 
   def index
+    redirect_to '/' if session[:user_id] == nil
+    @user = User.find(session[:user_id]).name
     @posts = Post.all
   end
 
