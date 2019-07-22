fa6360eda1f72480da4941e4f4861130455633f5
faisalleic <faisal.leic@yahoo.com>
2019-07-22 17:05:30 +0100

    Sign in works without correct validation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   resources :posts
   post '/users',     to: 'users#create'
-
+  get '/users/new',     to: 'users#new'
   root 'sessions#new'
 end
