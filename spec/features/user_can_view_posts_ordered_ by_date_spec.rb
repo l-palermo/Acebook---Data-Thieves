333ae195ab36bcbc586fe38e13e93301f4478d06
Luigi <l-palermo@hotmail.it>
2019-07-23 11:37:26 +0100

    development: test should pass

--- spec/features/user_can_view_posts_ordered_ by_date_spec.rb
+++ spec/features/user_can_view_posts_ordered_ by_date_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "User can view posts ordered by date" do
-    visit "/posts"
+    visit '/'
+    fill_in 'user_name',      with: 'test'
+    fill_in 'user_email',     with: 'test@test.com'
+    fill_in 'user_password',  with: 'testey'
+    click_button 'Create my account'
+    expect(current_path).to eq('/posts')
     click_link "New post"
     fill_in "Message", with: "This is message 1"
     click_button "Submit"
