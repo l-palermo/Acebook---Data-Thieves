08db249b915576726d83b87809bf6f7f32dcc7db
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-07-24 11:30:32 +0100

    Login tests pending, Fix in 13 minutes

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
+    visit '/users/new'
+    fill_in 'user_name',      with: 'test'
+    fill_in 'user_email',     with: 'test@test.com'
+    fill_in 'user_password',  with: 'testey'
+    click_button 'Create my account'
+    click_link "See all posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
