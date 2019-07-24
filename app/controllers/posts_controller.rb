e3cd146627507519805b9e828728a993e1442850
Student <student@MAKERSs-MacBook-Pro-89.local>
2019-07-24 11:46:53 +0100

    dev: ammended to meet acceptance criteria

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,17 +14,9 @@ class PostsController < ApplicationController
 
   def index
     redirect_to '/' if session[:user_id] == nil
-    @user = User.find(session[:user_id]) if session[:user_id] != nil
-    @posts = Post.all.order("created_at DESC")
-  end
-
-  def show
-    redirect_to '/' if session[:user_id] == nil
     @user = User.find(session[:user_id])
     @posts = Post.where(user_id: session[:user_id]).order("created_at DESC")
   end
-
-
 
   private
   def post_params
