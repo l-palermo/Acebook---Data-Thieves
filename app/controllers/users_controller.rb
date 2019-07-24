c3cea6b9ccf0f27f6f2989bb8c152d140f017d14
Student <student@MAKERSs-MacBook-Pro-89.local>
2019-07-24 14:44:08 +0100

    test: resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_posts_url(user)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_posts_url(@user)
     else
       render 'new'
     end
