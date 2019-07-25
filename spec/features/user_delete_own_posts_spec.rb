0cb68ca264f4387727154460ad8cc0d9536a4f5c
Student <student@MAKERSs-MacBook-Pro-89.local>
2019-07-25 15:40:28 +0100

    dev: error message raised on delete action

--- spec/features/user_delete_own_posts_spec.rb
+++ spec/features/user_delete_own_posts_spec.rb
@@ -16,4 +16,26 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content('Welcome test')
     expect(page).not_to have_content('Hello')
   end
+
+  scenario "Can submit posts and view them" do
+    visit '/users/new'
+    fill_in 'user_name',      with: 'test'
+    fill_in 'user_email',     with: 'test@test.com'
+    fill_in 'user_password',  with: 'testey'
+    click_button 'Create my account'
+    click_link "New post"
+    fill_in "Message", with: "Hello"
+    click_button "Submit"
+
+    visit '/users/new'
+    fill_in 'user_name',      with: 'test2'
+    fill_in 'user_email',     with: '2test@test.com'
+    fill_in 'user_password',  with: '2testey'
+    click_button 'Create my account'
+    click_link 'All Posts'
+    click_link 'Hello'
+    click_link 'Delete'
+    expect(page).to have_content("Error: you can\'t delete other users messages")
+    expect(page).to have_content('Hello')
+  end
 end
