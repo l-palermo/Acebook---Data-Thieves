dc052a174065cb99b3587868ae69c21dfbcccb9f
Luigi <l-palermo@hotmail.it>
2019-07-25 14:50:43 +0100

    dev: error raise, bad coded, have a look!

--- spec/features/user_delete_own_posts_spec.rb
+++ spec/features/user_delete_own_posts_spec.rb
@@ -16,4 +16,21 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content('Welcome test')
     expect(page).not_to have_content('Hello')
   end
+
+  scenario "Can submit posts and view them" do
+    visit '/users/new'
+    fill_in 'user_name',      with: 'test'
+    fill_in 'user_email',     with: 'test@test.com'
+    fill_in 'user_password',  with: 'testey'
+    click_button 'Create my account'
+    click_link "New post"
+    fill_in "Message", with: "Hello"
+    click_button "Submit"
+    click_link 'Hello'
+    click_link 'Delete'
+    expect(page).to have_content('Welcome test')
+    expect(page).not_to have_content('Hello')
+  end
+
+
 end
