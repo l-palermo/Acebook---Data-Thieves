30a170eb4c355d2ad9c657165c0b1d71c50b8bd1
Alexis Josephides <ajosephides@gmail.com>
2019-07-26 12:24:26 +0100

    feature: user cannot update a post if it was created more than 10 mins prior

--- spec/features/user_update_post_spec.rb
+++ spec/features/user_update_post_spec.rb
@@ -37,28 +37,21 @@ RSpec.feature "User cannot edit someone elses update a post", type: :feature do
   end
 end
 
-# RSpec.feature "User can update a post", type: :feature do
-#   scenario "User can update a post within 10 mins of creation" do
-#     # p "Time at begining: #{Time.now.utc}"
-#     user = User.create(name:"Jean-Claude", surname:"Van Damme", email:"doublehard@test.com", password:"1234567")
-#     post = user.posts.create(message:"All my plumbing is coconut water")
-#     # p "Post time: #{post.created_at}"
-#     # p "Time before moving forward: #{Time.now.utc}"
-#     # new_time = Time.now.utc + 11.minutes
-#     # Timecop.travel(new_time)
-#     # p "Time moving forward: #{Time.now.utc}"
-#     # p "Time difference between now and post: #{Time.now.utc - post.created_at}"
-#     # Timecop.return
-#     # p "Time back to normal: #{Time.now.utc}"
-#     visit '/'
-#     fill_in 'session_email', with: 'doublehard@test.com', visible: false
-#     fill_in 'session_password', with: '1234567', visible: false
-#     click_button 'Log in'
-#     click_link 'All my plumbing is coconut water'
-#     click_button 'Edit'
-#     fill_in "Message", with: "You see, I'm an ambitious Harvard-educated visionary who deserves to be the most powerful man in the world"
-#     click_button 'Update'
-#     expect(page).to have_content("You see, I'm an ambitious Harvard-educated visionary who deserves to be the most powerful man in the world")
-#     expect(page).not_to have_content("All my plumbing is coconut water")
-#   end
-# end
+RSpec.feature "User can update a post", type: :feature do
+  scenario "User can update a post within 10 mins of creation" do
+    user = User.create(name:"Jean-Claude", surname:"Van Damme", email:"doublehard@test.com", password:"1234567")
+    post = user.posts.create(message:"All my plumbing is coconut water")
+    new_time = Time.now.utc + 11.minutes
+    Timecop.travel(new_time)
+    visit '/'
+    fill_in 'session_email', with: 'doublehard@test.com', visible: false
+    fill_in 'session_password', with: '1234567', visible: false
+    click_button 'Log in'
+    click_link 'All my plumbing is coconut water'
+    click_link 'Edit'
+    fill_in "Message", with: "You see, I'm an ambitious Harvard-educated visionary who deserves to be the most powerful man in the world"
+    click_button 'Update'
+    expect(page).to have_content("Error: you can\'t update a post since it has been 10 minutes since it was created")
+    Timecop.return
+  end
+end
