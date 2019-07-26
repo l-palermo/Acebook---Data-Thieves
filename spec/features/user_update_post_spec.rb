cb1bd2cc458b4d4d49f9191a65c1ff16f554385a
Alexis Josephides <ajosephides@gmail.com>
2019-07-26 12:17:50 +0100

    feature: only a post owner can update their post

--- spec/features/user_update_post_spec.rb
+++ spec/features/user_update_post_spec.rb
@@ -17,6 +17,26 @@ RSpec.feature "User can update a post", type: :feature do
   end
 end
 
+
+RSpec.feature "User cannot edit someone elses update a post", type: :feature do
+  scenario "User cannot update someone else's post" do
+    user = User.create(name:"Jean-Claude", surname:"Van Damme", email:"doublehard@test.com", password:"1234567")
+    post = user.posts.create(message:"All my plumbing is coconut water")
+    user2 = User.create(name:"Dolph", surname:"Lungren", email:"iloveart@test.com", password:"1234567")
+    post2 = user2.posts.create(message:"You are discharged sarge")
+    visit '/'
+    fill_in 'session_email', with: 'doublehard@test.com', visible: false
+    fill_in 'session_password', with: '1234567', visible: false
+    click_button 'Log in'
+    click_link 'All Posts'
+    click_link 'You are discharged sarge'
+    click_link 'Edit'
+    fill_in "Message", with: "I will defeat you"
+    click_button 'Update'
+    expect(page).to have_content("Error: you can\'t update other users messages")
+  end
+end
+
 # RSpec.feature "User can update a post", type: :feature do
 #   scenario "User can update a post within 10 mins of creation" do
 #     # p "Time at begining: #{Time.now.utc}"
